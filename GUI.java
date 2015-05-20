bbb40b8e3f67d9a1720a5ee2c380511ee7c28bdf
apfarre5 <apfarre5@students.d125.org>
2015-05-20 09:17:13 -0500

    add GUI

---
+++ GUI.java
@@ -0,0 +1,104 @@
+
+import java.awt.*;
+import javax.swing.*;
+
+public class GUI extends JApplet
+{
+
+    public void init()
+    {
+        // this is a workaround for a security conflict with some browsers
+        // including some versions of Netscape & Internet Explorer which do 
+        // not allow access to the AWT system event queue which JApplets do 
+        // on startup to check access. May not be necessary with your browser. 
+        JRootPane rootPane = this.getRootPane();    
+        rootPane.putClientProperty("defeatSystemEventQueueCheck", Boolean.TRUE);
+
+        // provide any initialisation necessary for your JApplet
+    }
+
+    public void start()
+    {
+        // provide any code requred to run each time 
+        // web page is visited
+    }
+
+    public void stop()
+    {
+        // provide any code that needs to be run when page
+        // is replaced by another page or before JApplet is destroyed 
+    }
+
+    public void paint(Graphics g)
+    {
+        int cellX=50;
+        int cellY=50;
+        
+        int[][] array = new int[9][9];
+        
+        //selected cell
+        g.setColor(Color.magenta);
+        g.fillRect(cellX,cellY,60,60);
+        
+        // simple text displayed on applet
+        g.setColor(Color.black);
+        g.fillRect(0,0,50,590);
+        g.fillRect(590,0,50,590);
+        g.fillRect(0,0,590,50);
+        g.fillRect(0,590,640,50);
+        for(int ctr=50; ctr<=600;ctr+=60)
+        {
+            g.drawLine(ctr,0,ctr,590);
+        }
+        for(int ctr=50; ctr<=600;ctr+=60)
+        {
+            g.drawLine(0,ctr,590,ctr);
+        }
+        g.fillRect(227,0,6,590);
+        g.fillRect(407,0,6,590);
+        g.fillRect(0,227,590,6);
+        g.fillRect(0,407,590,6);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        //         g.setColor(Color.black);
+        //         g.drawString("Sample Applet", 20, 20);
+        //         g.setColor(Color.blue);
+        //         g.drawString("created by BlueJ", 20, 40);
+    }
+
+    public void destroy()
+    {
+        // provide code to be run when JApplet is about to be destroyed.
+    }
+
+    
+    public String getAppletInfo()
+    {
+        // provide information about the applet
+        return "Title:   \nAuthor:   \nA simple applet example description. ";
+    }
+
+    
+    public String[][] getParameterInfo()
+    {
+        // provide parameter information about the applet
+        String paramInfo[][] = {
+                {"firstParameter",    "1-10",    "description of first parameter"},
+                {"status", "boolean", "description of second parameter"},
+                {"images",   "url",     "description of third parameter"}
+            };
+        return paramInfo;
+    }
+}
